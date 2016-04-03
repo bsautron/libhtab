6ac5dd5a81949af6c4ec4bc916c3ba064f1893e3
Bruno SAUTRON <bsautron@e2r4p18.42.fr>
2016-04-03 19:43:51 +0200

    Norme header

--- includes/libhtab.h
+++ includes/libhtab.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libhtab.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsautron <bsautron@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/03 19:42:17 by bsautron          #+#    #+#             */
+/*   Updated: 2016/04/03 19:43:02 by bsautron         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBHTAB_H
 # define LIBHTAB_H
 
@@ -5,27 +17,27 @@
 # include <liblist.h>
 
 # define HTAB_CREATE(Y, Z)	create_htab(Y, Z)
-# define HTAB_SET(Y, Z)			htab_set(Y, (t_dhtab *)Z)
+# define HTAB_SET(Y, Z)		htab_set(Y, (t_dhtab *)Z)
 # define HTAB_GET(X, Y, Z)	(Y *)htab_get(X, Z)
 
 typedef struct	s_dhtab
 {
-	void		*next;
-	char		*key;
+	void	*next;
+	char	*key;
 }				t_dhtab;
 
 typedef struct	s_htab
 {
 	t_dhtab	**array;
-	char		*type;
-	int			length;
-	int			length_max;
-	int			(*hash)(char *);
+	char	*type;
+	int		length;
+	int		length_max;
+	int		(*hash)(char *);
 }				t_htab;
 
-int		hash_pour_les_nuls(char *s);
-t_htab	create_htab(int length_max, int (*hash)(char *));
-void	htab_set(t_htab *htab, t_dhtab *value);
-t_dhtab	*htab_get(t_htab htab, char *key);
+int				hash_pour_les_nuls(char *s);
+t_htab			create_htab(int length_max, int (*hash)(char *));
+void			htab_set(t_htab *htab, t_dhtab *value);
+t_dhtab			*htab_get(t_htab htab, char *key);
 
 #endif
